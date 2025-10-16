2599f05226c2a0258d1a20fe5c45d735f958753d
Dev Mushthak <devmusthak@gmail.com>
2025-10-16 17:23:56 +0530

    new one

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import RoleSelection from './pages/RoleSelection.jsx'
+
+function App() {
+  return (
+    <>
+      <RoleSelection />
+    </>
+  )
+}
+
+export default App
